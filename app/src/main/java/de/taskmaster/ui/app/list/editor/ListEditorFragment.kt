b2764bea0fef003fcaeb033f20504b502259ea08
Bengt Joachimsohn <bengt.joachimsohn@bewotec.de>
2021-01-28 14:59:08 +0100

    update
    - reworked Listfragment view to have a more modern and compliant style

--- app/src/main/java/de/taskmaster/ui/app/list/editor/ListEditorFragment.kt
+++ app/src/main/java/de/taskmaster/ui/app/list/editor/ListEditorFragment.kt
@@ -5,51 +5,96 @@ import android.view.View
 import android.widget.Button
 import android.widget.DatePicker
 import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
-import androidx.navigation.fragment.findNavController
 import de.taskmaster.R
+import de.taskmaster.R.string.edit
 import de.taskmaster.ui.app.SubFragment
 import java.time.LocalDateTime
 
 class ListEditorFragment : SubFragment(R.layout.fragment_list_edit) {
 
-    val model = ListEditorViewModel()
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        view.findViewById<Button>(R.id.recurring).setOnClickListener {
-            findNavController().navigate(R.id.action_list_editor_to_repeatingEventFragment)
-        }
-
-        val pickerLayout = view.findViewById<LinearLayout>(R.id.deadline_picker_layout)
         val picker = view.findViewById<DatePicker>(R.id.deadline_picker)
-        val deadlineDate = view.findViewById<TextView>(R.id.deadline_date)
-        picker.setOnDateChangedListener { _, year, monthOfYear, dayOfMonth ->
-            deadlineDate.text = "${dayOfMonth}/${monthOfYear}/${year}"
-        }
 
         view.findViewById<Button>(R.id.date_today).setOnClickListener {
             val current = LocalDateTime.now()
             picker.updateDate(current.year, current.monthValue, current.dayOfMonth)
         }
         view.findViewById<Button>(R.id.date_tomorrow).setOnClickListener {
-            val current = LocalDateTime.now().plusDays(1L)
-            picker.updateDate(current.year, current.monthValue, current.dayOfMonth)
+            val tomorrow = LocalDateTime.now().plusDays(1L)
+            picker.updateDate(tomorrow.year, tomorrow.monthValue, tomorrow.dayOfMonth)
         }
         view.findViewById<Button>(R.id.date_next_week).setOnClickListener {
-            val current = LocalDateTime.now().plusDays(7L)
-            picker.updateDate(current.year, current.monthValue, current.dayOfMonth)
+            val nextWeek = LocalDateTime.now().plusDays(7L)
+            picker.updateDate(nextWeek.year, nextWeek.monthValue, nextWeek.dayOfMonth)
         }
-        view.findViewById<Button>(R.id.deadline_button).setOnClickListener {
+
+        val test = view.findViewById<LinearLayout>(R.id.place_linearLayout)
+        test.addView(TextView(context).apply { text = "FICKEEEEN" })
+
+        val toggle = view.findViewById<TextView>(R.id.edit_deadline)
+        toggle.setOnClickListener {
+            val pickerLayout = view.findViewById<LinearLayout>(R.id.lower_deadline)
             pickerLayout.visibility = when (pickerLayout.visibility) {
-                View.GONE -> View.VISIBLE
-                View.VISIBLE -> View.GONE
+                View.GONE -> {
+                    toggle.text = getString(R.string.select)
+                    View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    toggle.text = getString(edit)
+                    View.GONE
+                }
                 else -> error("not supposed to happen")
             }
         }
 
-        view.findViewById<Button>(R.id.place).setOnClickListener {
-            findNavController().navigate(R.id.action_list_editor_to_mapsFragment)
+        val toggle2 = view.findViewById<TextView>(R.id.edit_repeating)
+        toggle2.setOnClickListener {
+            val pickerLayout = view.findViewById<LinearLayout>(R.id.lower_repeating)
+            pickerLayout.visibility = when (pickerLayout.visibility) {
+                View.GONE -> {
+                    toggle2.text = getString(R.string.select)
+                    View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    toggle2.text = getString(edit)
+                    View.GONE
+                }
+                else -> error("not supposed to happen")
+            }
+        }
+
+        val toggle3 = view.findViewById<TextView>(R.id.edit_place)
+        toggle3.setOnClickListener {
+            val pickerLayout = view.findViewById<ScrollView>(R.id.lower_place)
+            pickerLayout.visibility = when (pickerLayout.visibility) {
+                View.GONE -> {
+                    toggle3.text = getString(R.string.select)
+                    View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    toggle3.text = getString(edit)
+                    View.GONE
+                }
+                else -> error("not supposed to happen")
+            }
+        }
+
+        val toggle4 = view.findViewById<TextView>(R.id.edit_group)
+        toggle4.setOnClickListener {
+            val pickerLayout = view.findViewById<LinearLayout>(R.id.lower_group)
+            pickerLayout.visibility = when (pickerLayout.visibility) {
+                View.GONE -> {
+                    toggle4.text = getString(R.string.select)
+                    View.VISIBLE
+                }
+                View.VISIBLE -> {
+                    toggle4.text = getString(edit)
+                    View.GONE
+                }
+                else -> error("not supposed to happen")
+            }
         }
     }
-
 }
--- app/src/main/java/de/taskmaster/ui/app/list/editor/subeditors/RepeatingEventFragment.kt
+++
@@ -1,6 +0,0 @@
-package de.taskmaster.ui.app.list.editor.subeditors
-
-import de.taskmaster.R
-import de.taskmaster.ui.app.SubFragment
-
-class RepeatingEventFragment : SubFragment(R.layout.fragment_list_repeating)
